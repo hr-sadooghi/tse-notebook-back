b9b8af610d156222598542ca0c66edcac0a46353
hr-sadooghi <h62asl@gmail.com>
2019-06-19 15:41:23 +0430

    Make Company Entity. Implement: /company*, /users Rest APIs

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/namads', function () {
-    return App\Namad::all();
+Route::get('/user', function () {
+    $user_id = auth()->id();
+    /** @var \App\User $user */
+    $user = App\User::find($user_id);
+    return response($user, 200);
 });
+
+
+Route::get('/companies', 'CompanyController@apiGetAllCompanies');
+Route::get('/companies/favorites', 'CompanyController@apiGetAllCompaniesAndFavoriteState');
+Route::get('/users/favorites/companies', 'CompanyController@apiGetCurrentUserFavoriteCompanies');
+Route::post('/users/favorites/companies/{company_id}', 'CompanyController@apiPostAddCompanyToCurrentUserFavorite');
+Route::delete('/users/favorites/companies/{company_id}', 'CompanyController@apiDeleteRemoveCompanyFromCurrentUserFavorite');
+
+
