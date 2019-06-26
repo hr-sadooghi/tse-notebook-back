c24a83364dc8627948a728a26eb4ff3f60d12a73
hr-sadooghi <h62asl@gmail.com>
2019-06-26 22:41:11 +0430

    Add models, migrations and controllers for: Event, File, Link, Trade Implement: LinkController, FileController File upload with thumbnail build.

---
+++ database/migrations/2019_06_24_145235_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('company_id');
+            $table->text('description');
+            $table->enum('type', ['text', 'image', 'file', 'link']);
+            $table->date('date');
+            $table->string('detail_type')->nullable();
+            $table->unsignedBigInteger('detail_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
